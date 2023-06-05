6965635eb8d72ac3eab86419c76d1ad040954b26
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-06-05 22:34:13 +0300

    Fix pipeline

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "/keybraker.github.io/" : "",
+  assetPrefix: isProd ? "/medwork.gr/" : "",
   images: {
     unoptimized: true,
   },
