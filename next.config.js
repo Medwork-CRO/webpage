75cc4e2ca58381a42a95f53d8244e9183c721b87
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-06-06 21:24:30 +0300

    Add assetPrefix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "/medwork.gr/" : "",
+  assetPrefix: isProd ? "/medwork.gr/" : "itsiakkas.xyz",
   images: {
     unoptimized: true,
   },
