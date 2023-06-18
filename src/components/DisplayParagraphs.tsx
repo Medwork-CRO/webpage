5a6fa47108a2f87d8fe1a4425701c8366dbb06cf
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-06-18 20:38:37 +0300

    Fix pipeline

--- src/components/DisplayParagraphs.tsx
+++ src/components/DisplayParagraphs.tsx
@@ -6,7 +6,7 @@ function DisplayParagraphs({ paragraphs }: DisplayParagraphsProps) {
   return (
     <div className="flex flex-col gap-8 whitespace-pre-line text-xl text-gray-500 dark:text-gray-400 px-4 sm:px-8 md:px-16 lg:px-24 md:text-2xl">
       {paragraphs.map((paragraph, index) => (
-        <p className={`${!index ? "font-semibold" : ""}`}>{paragraph}</p>
+        <p key={index} className={`${!index ? "font-semibold" : ""}`}>{paragraph}</p>
       ))}
     </div>
   );
