554723c87af24759f6d39d4da30d14a3a54a5704
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-07-02 23:06:44 +0300

    Update image showcase

--- src/components/ImageShowCase.tsx
+++ src/components/ImageShowCase.tsx
@@ -18,10 +18,11 @@ function ImageShowCase({
   link,
 }: ImageShowCaseProps) {
   return (
-    <div className="flex flex-col sm:flex-row h-[815px] w-full flex-[0_0_100%]">
+    <div className="flex flex-col md:flex-row h-[815px] w-full flex-[0_0_100%]">
       {/* <div className="absolute inset-0 z-10 bg-cyan-800 opacity-70"></div> */}
 
-      <div className="flex flex-col w-1/3 items-center justify-center gap-8 sm:ml-16 text-gray-500 dark:text-gray-400">
+      <Image src={image} className="h-full w-2/3 object-cover" alt="alt" />
+      <div className="flex flex-col w-1/3 items-center justify-center gap-8 sm:pl-8 sm:pr-20 text-gray-500 dark:text-gray-400">
         <div className="flex">
           <span
             className={`my-4 text-center text-7xl font-bold italic`}
@@ -32,12 +33,11 @@ function ImageShowCase({
             {title}
           </span>
         </div>
-        <span className="text-center text-xl font-semibold md:text-4xl xl:text-6xl">
+        <span className="align-left text-2xl font-semibold md:text-4xl xl:text-6xl">
           {description}
         </span>
         <span className="text-xs md:text-xl xl:text-2xl">{description2}</span>
       </div>
-      <Image src={image} className="h-full w-2/3 object-cover" alt="alt" />
     </div>
   );
 }
