5a3f75758bbd632f31c6326eba51c56ad67361fe
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-07-02 22:57:24 +0300

    Made showcase texts larger

--- src/components/ImageShowCase.tsx
+++ src/components/ImageShowCase.tsx
@@ -21,8 +21,8 @@ function ImageShowCase({
     <div className="flex flex-col sm:flex-row h-[815px] w-full flex-[0_0_100%]">
       {/* <div className="absolute inset-0 z-10 bg-cyan-800 opacity-70"></div> */}
 
-      <div className="flex flex-col w-1/3 gap-8 sm:ml-16 text-gray-500 dark:text-gray-400">
-        <div className="flex items-center justify-center">
+      <div className="flex flex-col w-1/3 items-center justify-center gap-8 sm:ml-16 text-gray-500 dark:text-gray-400">
+        <div className="flex">
           <span
             className={`my-4 text-center text-7xl font-bold italic`}
           // className={`${inter.variable} dark:text-white text-xl font-semibold text-gray-400 xs:text-3xl`}
@@ -32,7 +32,7 @@ function ImageShowCase({
             {title}
           </span>
         </div>
-        <span className="text-center text-xl font-semibold md:text-3xl xl:text-5xl">
+        <span className="text-center text-xl font-semibold md:text-4xl xl:text-6xl">
           {description}
         </span>
         <span className="text-xs md:text-xl xl:text-2xl">{description2}</span>
