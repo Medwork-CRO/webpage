d8b0474231b289c8c02c21bdaa7dca472100c4c6
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-07-05 18:42:03 +0300

    Update wrong white text to cyan-950 on info card

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -9,7 +9,10 @@ type InfoBoxProps = {
 
 const InfoBox = ({ title, text, link, color }: InfoBoxProps) => (
   <Link href={link}>
-    <div className={`flex flex-col p-8 ${color} h-[18rem] sm:h-[28rem] lg:h-[30rem] w-full md:max-w-[26rem] lg:max-w-[18rem] saturate-50 hover:saturate-150 hover:border-cyan-600 border-4 border-transparent text-cyan-900 transition duration-500 ease-in-out`}>
+    <div className={`
+      flex flex-col p-8 ${color} h-[18rem] sm:h-[28rem] lg:h-[30rem]
+      w-full md:max-w-[26rem] lg:max-w-[18rem] saturate-50 hover:saturate-150
+      hover:border-cyan-600 border-4 border-transparent text-cyan-950 transition duration-500 ease-in-out`}>
       <h4 className="mb-2 text-xl sm:text-2xl font-bold">{title}</h4>
       <p className="text-md sm:text-lg font-semibold">{text}</p>
     </div>
