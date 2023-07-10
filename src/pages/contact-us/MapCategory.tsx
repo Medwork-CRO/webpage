97764e439ccc9ac4b60acdb320b4595a98813dd9
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-07-10 11:59:27 +0300

    Fix contact page design

--- src/pages/contact-us/MapCategory.tsx
+++ src/pages/contact-us/MapCategory.tsx
@@ -1,7 +1,12 @@
-import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
-import { AiOutlinePhone, AiOutlineMail, AiOutlineEnvironment } from "react-icons/ai";
-import Title from "../../components/Title";
+import { AiOutlineEnvironment, AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+
+enum ContactType {
+  Phone,
+  Fax,
+  Email,
+  Address,
+}
 
 type MapCategoryProps = {
   location?: string;
@@ -12,58 +17,62 @@ function getIFrame(location: string, rounded = false) {
   const rounding = rounded ? 'rounded-lg' : '';
   return (<iframe
     src={`https://maps.google.com/maps?q=${location}&t=&z=16&ie=UTF8&iwloc=&output=embed`}
-    className={`w-full h-[420px] ${rounding}
-     border-t-4 border-t-[#2d333b] dark:border-t-[#f6f1eb]
-    border-b-4 border-b-[#2d333b] dark:border-b-[#f6f1eb]
-    border-r-4 border-r-[#2d333b] dark:border-r-[#f6f1eb]`}
+    className={`w-full h-[420px] ${rounding}`}
     allowFullScreen
   ></iframe>);
 }
 
+function getIcon(contactType: ContactType) {
+  switch (contactType) {
+    case ContactType.Phone:
+      return <AiOutlinePhone className="h-6 w-6" />;
+    case ContactType.Fax:
+      return <AiOutlinePhone className="h-6 w-6" />;
+    case ContactType.Email:
+      return <AiOutlineMail className="h-6 w-6" />;
+    case ContactType.Address:
+      return <AiOutlineEnvironment className="h-6 w-6" />;
+  }
+}
+
+function getContact(display: string, value: string, contactType: ContactType) {
+  let href = "";
+
+  switch (contactType) {
+    case ContactType.Phone:
+      href = `tel:${value}`;
+      break;
+    case ContactType.Fax:
+      href = `fax:${value}`;
+      break;
+    case ContactType.Email:
+      href = `mailto:${value}`;
+      break;
+    case ContactType.Address:
+      href = `https://goo.gl/maps/${value}`;
+      break;
+  }
+
+  return (<div className="flex gap-4 items-center rounded-lg backdrop-blur-3xl px-3 py-3">
+    {getIcon(contactType)}
+    <Link
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {display}
+    </Link>
+  </div>);
+}
+
 function getCompanyInfo() {
   return (
-    <div className="font-semibold leading-[2rem] sm:leading-[2rem] md:leading-[3rem] xl:leading-[4rem] text-lg sm:text-xl text-[#2d333b] dark:text-[#f6f1eb] text-left">
-      <div className="text-[#2d333b] dark:text-[#f6f1eb] flex flex-col justify-end">
-        <div className="mb-6 flex gap-4 items-center rounded-full backdrop-blur-3xl border-white border-2 px-6 py-2">
-          <AiOutlinePhone className="h-6 w-6" />
-          <Link
-            href="tel:+302109960971"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            (+30) 210 9960971
-          </Link>
-        </div>
-        <div className="mb-6 flex gap-4 items-center rounded-full backdrop-blur-3xl border-white border-2 px-6 py-2">
-          <AiOutlinePhone className="h-6 w-6" />
-          <Link
-            href="fax:+302109969578"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            (+30) 210 9969578
-          </Link>
-        </div>
-        <div className="mb-6 flex gap-4 items-center rounded-full backdrop-blur-3xl border-white border-2 px-6 py-2">
-          <AiOutlineMail className="h-6 w-6" />
-          <Link
-            href="mailto:medworkinfo@medwork.gr"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            medworkinfo@medwork.gr
-          </Link>
-        </div>
-        <div className="flex gap-4 items-center rounded-full backdrop-blur-3xl border-white border-2 px-6 py-2">
-          <AiOutlineEnvironment className="h-6 w-6" />
-          <Link
-            href="https://goo.gl/maps/KDKk5piro9Cb6AXG7"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Chiou 5, Argyroupoli 16452
-          </Link>
-        </div>
+    <div className="font-semibold text-xl text-[#2d333b] dark:text-[#f6f1eb] text-left">
+      <div className="text-[#2d333b] dark:text-[#f6f1eb] flex flex-col justify-end gap-4">
+        {getContact('(+30) 210 9960971', '+302109960971', ContactType.Phone)}
+        {getContact('(+30) 210 9969578', '+302109969578', ContactType.Fax)}
+        {getContact('medworkinfo@medwork.gr', 'medworkinfo@medwork.gr', ContactType.Email)}
+        {getContact('Chiou 5, Argyroupoli 16452', 'KDKk5piro9Cb6AXG7', ContactType.Address)}
       </div>
     </div>
   );
