8c3fbd1e9da1a0937c7ba1a9bb44c060ff23a37f
Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-11-27 21:36:06 +0200

    Update company brief in CompanyInfo component

--- src/components/companyInfo.tsx
+++ src/components/companyInfo.tsx
@@ -5,7 +5,7 @@ type CompanyInfoProps = {
   align?: "center" | "left";
 }
 
-const companyBriefOld = "Medwork is a Contract Research Organization (CRO) operating in Southeast Europe, which since 2005 offers a wide range of specialized services to the pharmaceutical and medical device industries.";
+// const companyBriefOld = "Medwork is a Contract Research Organization (CRO) operating in Southeast Europe, which since 2005 offers a wide range of specialized services to the pharmaceutical and medical device industries.";
 const companyBrief = "Since its establishment in 2005, Medwork has been offering an extensive array of specialised vigilance services to the pharmaceutical and medical device industries.";
 
 function CompanyInfo({ align = "left" }: CompanyInfoProps) {
