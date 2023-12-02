b355c004a1fac3d871b42559debdcdf59857aae0
Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-12-02 07:10:35 +0200

    Refactor title and description rendering in CarouselImage and ImageCategory components

--- src/components/CarouselImage.tsx
+++ src/components/CarouselImage.tsx
@@ -51,13 +51,13 @@ function CarouselImage({ image, title }: ImageShowCaseProps) {
             {/* <div className="absolute inset-x-0 top-0 z-10 from-medwork-light dark:from-medwork-dark to-transparent bg-gradient-to-b h-[6%]"></div> */}
             <div className="absolute inset-x-0 bottom-0 z-10 from-medwork-light dark:from-medwork-dark to-transparent bg-gradient-to-t h-5/6"></div>
 
-            <div className="absolute bottom-8 sm:bottom-16 z-20 w-full mb-8 sm:mb-2">
+            {title && <div className="absolute bottom-8 sm:bottom-16 z-20 w-full mb-8 sm:mb-2">
                 <div className="flex flex-col justify-center gap-8 max-w-[1200px] px-4 md:px-6 lg:px-0 mx-auto text-gray-700 dark:text-gray-300 text-center">
                     <span className="font-sans font-light narrow-letters text-3xl sm:text-4xl lg:text-5xl leading-normal  sm:leading-normal md:leading-normal lg:leading-normal">
                         {title}
                     </span>
                 </div>
-            </div>
+            </div>}
         </div>
     );
 }
--- src/components/ImageCategory.tsx
+++ src/components/ImageCategory.tsx
@@ -26,9 +26,9 @@ function ImageCategory({
 
                 <div className="absolute bottom-8 sm:bottom-16 z-20 w-full mb-8 sm:mb-2">
                     <div className="flex flex-col justify-center gap-8 max-w-[1200px] px-4 md:px-6 lg:px-0 mx-auto text-gray-700 dark:text-gray-300 text-center">
-                        <div className="mt-4 md:mt-0">
-                            <h1 className="mb-2 text-2xl sm:text-6xl">{title}</h1>
-                            <p className="mb-4 text-md sm:text-3xl">{description}</p>
+                        <div className="flex flex-col gap-4 mt-4 md:mt-0">
+                            <h1 className="text-4xl sm:text-6xl">{title}</h1>
+                            {description && <p className="mb-4 text-md sm:text-3xl">{description}</p>}
                         </div>
                     </div>
                 </div>
