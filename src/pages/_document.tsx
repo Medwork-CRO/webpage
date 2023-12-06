5ff594c95fb44854e46446499f0fd0bdb665e1e9
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2023-12-06 22:34:36 +0200

    Add vercel analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Head, Html, Main, NextScript } from "next/document";
 
 function Layout() {
@@ -7,6 +8,7 @@ function Layout() {
             <body>
                 <Main />
                 <NextScript />
+                <Analytics />
             </body>
         </Html>
     );
