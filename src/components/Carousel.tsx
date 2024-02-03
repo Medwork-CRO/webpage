3bcfbb7296fc6434d39f28f3b969f6d6033a7edd
ryzenbeast\Ioannis Tsiakkas <iantsiakkas@gmail.com>
2024-02-03 20:24:12 +0200

    Add h1 tags to all pages

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -38,9 +38,9 @@ const Carousel = ({ children, title }: Props) => {
             <div className="flex">{children}</div>
             <div className="absolute bottom-8 sm:bottom-16 z-20 w-full mb-24 sm:mb-48">
                 <div className="flex flex-col justify-center gap-8 max-w-[1200px] px-4 md:px-6 eq:px-0 mx-auto text-gray-700 dark:text-gray-300 text-center">
-                    <span className="font-sans font-light narrow-letters text-3xl sm:text-4xl lg:text-5xl leading-normal sm:leading-normal md:leading-normal lg:leading-normal">
+                    <h1 className="font-sans font-light narrow-letters text-3xl sm:text-4xl lg:text-5xl leading-normal sm:leading-normal md:leading-normal lg:leading-normal">
                         {title}
-                    </span>
+                    </h1>
                 </div>
             </div>
 
--- src/components/ImageCategory.tsx
+++ src/components/ImageCategory.tsx
@@ -23,14 +23,14 @@ function ImageCategory({
             {/* <div className="absolute inset-x-0 bottom-0 z-10 h-full bg-medwork-dark bg-opacity-60"></div> */}
             {/* <div className="absolute inset-x-0 bottom-0 z-10 from-medwork-light dark:from-medwork-dark to-transparent bg-gradient-to-t h-5/6"></div> */}
 
-            {/* <div className="absolute bottom-8 sm:bottom-16 z-20 w-full mb-8 sm:mb-2">
+            <div className="absolute bottom-8 sm:bottom-16 z-20 w-full mb-8 sm:mb-2">
                 <div className="flex flex-col justify-center gap-8 w-full max-w-[1200px] px-4 md:px-6 eq:px-0 mx-auto text-medwork-light text-start">
                     <div className="flex flex-col gap-4 mt-4 md:mt-0">
-                        <h1 className="text-4xl sm:text-6xl">{title}</h1>
+                        <h1 className="text-4xl sm:text-6xl hidden">{title}</h1>
                         {description && <p className="mb-4 text-md sm:text-3xl">{description}</p>}
                     </div>
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 }
